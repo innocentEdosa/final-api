cd1d3f7e7eba1757920fda478cbb0d30e3c0d25a
innocentEdosa <ilegbinijieinnocentcareer@gmail.com>
2019-01-04 08:10:47 +0100

    Setup POST/meetups endpoint

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//  import routers
+const meetupRoutes = require('./routes/meetup');
+
+// middleware
+app.use('/api/v1/meetups', meetupRoutes);
+
 const server = http.createServer(app);
 server.listen(process.env.PORT || 3004);
 module.exports = server;
